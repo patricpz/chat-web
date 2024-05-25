d23cdf8aafc026bfeba8a855c8f293b1380c6412
patricpz <patricbarros19@gmail.com>
2024-05-24 21:31:06 -0300

    config do firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,13 +5,13 @@ import "firebase/compat/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-  measurementId: ""
+  apiKey: "AIzaSyASdguljdKf4QdgdpxXtwvRsoCwAYM0QFg",
+  authDomain: "chat-web-41a2c.firebaseapp.com",
+  projectId: "chat-web-41a2c",
+  storageBucket: "chat-web-41a2c.appspot.com",
+  messagingSenderId: "390868360276",
+  appId: "1:390868360276:web:507a17330b2dd7d87061c1",
+  measurementId: "G-BS58MGF20F"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
